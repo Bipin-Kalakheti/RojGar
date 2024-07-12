74e95e909b677e5b71f8051095ecdbfb2cefc888
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-07-12 15:40:29 -0400

    chore: Update npm dependencies and add lucide-react for icons

---
+++ utils/links.tsx
@@ -0,0 +1,25 @@
+import { Plus, Briefcase, AreaChart } from "lucide-react";
+
+type NavLink = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+};
+
+const links: Array<NavLink> = [
+  {
+    href: "/add-job",
+    label: "Add Job",
+    icon: <Plus />,
+  },
+  {
+    href: "/jobs",
+    label: "Jobs",
+    icon: <Briefcase />,
+  },
+  {
+    href: "/stats",
+    label: "Stats",
+    icon: <AreaChart />,
+  },
+];
